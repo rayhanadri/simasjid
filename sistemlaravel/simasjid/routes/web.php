27c53222e9b8913f186306099a9c329d7454c537
rayhanadri <rayerzz99@gmail.com>
2019-10-04 01:38:16 +0700

    Tambah view LoginPage, edit router, tambah LoginController

--- sistemlaravel/simasjid/routes/web.php
+++ sistemlaravel/simasjid/routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/auth', 'UserController@login')->name('login');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
