ed9a13f313edb8ce0f24dc1b4fde10d6a3a0b7dc
rayhanadri <rayerzz99@gmail.com>
2019-10-18 18:23:44 +0700

    reset password fix

--- sistemlaravel/simasjid/resources/lang/en/passwords.php
+++ sistemlaravel/simasjid/resources/lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'password' => 'Passwords must be at least eight characters and match the confirmation.',
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have e-mailed your password reset link!',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that e-mail address.",
+    'password' => 'Password harus minimal 8 karakter dan sesuai dengan konfirmasi password.',
+    'reset' => 'Password Anda telah direset!',
+    'sent' => 'Kami telah mengirimkan email reset password Anda!',
+    'token' => 'Token password ini Invalid.',
+    'user' => "Kami tidak dapat menemukan pengguna dengan alamat email tersebut.",
 
 ];
