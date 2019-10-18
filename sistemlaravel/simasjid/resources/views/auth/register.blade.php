a40daa7077da6ab4e5b04ac7196e0eabde1e2087
rayhanadri <rayerzz99@gmail.com>
2019-10-18 20:30:29 +0700

    fix typo regist

--- sistemlaravel/simasjid/resources/views/auth/register.blade.php
+++ sistemlaravel/simasjid/resources/views/auth/register.blade.php
@@ -94,7 +94,7 @@
                   <div class="form-group row">
                     <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('No Telp/HP') }}</label>
                     <div class="col-md-6">
-                      <input id="telp" type="text" class="form-control @error('telp') is-invalid @enderror" name="telp" value="{{ old('telp') }}" autocomplete="telp" placeholder="Np. Telepon/HP">
+                      <input id="telp" type="text" class="form-control @error('telp') is-invalid @enderror" name="telp" value="{{ old('telp') }}" autocomplete="telp" placeholder="No. Telepon/HP">
                       @error('telp')
                       <span class="invalid-feedback" role="alert">
                         <strong>{{ $message }}</strong>
